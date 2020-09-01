54a7ee0bfad05fdc07efcc4d84ffafe89eca74be
LWRGitHub <LoganReynoldsLWR@gmail.com>
2020-09-01 11:58:29 -0700

    hw 2 commit

---
+++ hw-2-functions/functions.py
@@ -0,0 +1,28 @@
+#area function
+def area(width, height):
+    result = width * height
+    return result
+
+result = area(5, 6)
+print(result)
+
+print(area(8, 5))
+print(area(10, 3))
+
+#subtraction funcition
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+print(subtract(7, 5))
+print(subtract(200, 10))
+
+#add fucntion
+def add(num1, num2):
+    return num1 + num2
+
+print(add(2, 2))
+print(add(4, 9))
+
+
+
