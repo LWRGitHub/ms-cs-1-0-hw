350d559715534aca632dbc0e088d6bb51e73b80b
LWRGitHub <LoganReynoldsLWR@gmail.com>
2020-09-28 19:20:56 -0700

    strech added

--- hw-poetry/poetry-slam.py
+++ hw-poetry/poetry-slam.py
@@ -1,5 +1,6 @@
 import random
 
+#Q1
 def get_file_lines(filename):
     """ gets file & puts it in a list """
     file_line = open(filename, 'r')
@@ -7,6 +8,7 @@ def get_file_lines(filename):
 
     return lines_list
 
+#Q2
 def lines_printed_backwards(lines_list):
     """ prints lines backwards with line number """
     num_list = range(len(lines_list))
@@ -15,6 +17,7 @@ def lines_printed_backwards(lines_list):
     for num in reversed(num_list):
         print(f"{num_list[num]} {lines_list[num]}")
 
+#Q3
 def lines_printed_random(lines_list):
     """ prints random lines """
     num_list = range(len(lines_list))
@@ -24,7 +27,7 @@ def lines_printed_random(lines_list):
         num = random.randrange(10)
         print(lines_list[num])
   
-
+#Q4
 def lines_printed_custom(lines_list):
     """ every other letter is capital """
     for line in lines_list:
@@ -39,17 +42,53 @@ def lines_printed_custom(lines_list):
                 string += line[i]
         print(string)
 
-# def get_file_lines():
+#S1
+def stretch_write_file(lines_list):
+    file_name = f"{input('Please enter a file name for the file: ')}.txt"
+    f = open(file_name, "w")
+
+    for line in lines_list: 
+        f.write(line)
+    print('file created!')
+
+#S3
+# def stretch_read_poem_from_input():
+#     file_name = f"{input('Please enter a file name for the file: ')}.txt"
+#     f = open(file_name, "w")
+
+#     poem = input("Type you poem here: ")
+#     f.write(poem)
+
+#     file_line = open(file_name, 'r')
+#     lines_list = file_line.readlines()
+#     return lines_list
+
+#S2
+def stretch_menu():
+    poem_list = get_file_lines("poem.txt")
+
+    answer = input('Enter "backwards", "random", "write poem to file" or "custom" to see the poem printed in that way. Or type "done" to exit: ')
+
+    while answer != "done":
+        
+        if answer == "backwards":
+            lines_printed_backwards(poem_list)
+            answer = input('Enter "backwards", "random", "write poem to file" or "custom" to see the poem printed in that way. Or type "done" to exit: ')
+        elif answer == "random":
+            print(lines_printed_random(poem_list))
+            answer = input('Enter "backwards", "random", "write poem to file" or "custom" to see the poem printed in that way. Or type "done" to exit: ')
+        elif answer == "custom":
+            print(lines_printed_custom(poem_list))
+            answer = input('Enter "backwards", "random", "write poem to file" or "custom" to see the poem printed in that way. Or type "done" to exit: ')
+        elif answer == "write poem to file":
+            stretch_write_file(poem_list)
+            answer = input('Enter "backwards", "random", "write poem to file" or "custom" to see the poem printed in that way. Or type "done" to exit: ')
+        #S3 
+        # elif answer == "input peom":
+        #     poem_list = stretch_read_poem_from_input()
+        #     answer = input('Enter "backwards", "random", "write poem to file" or "custom" to see the poem printed in that way. Or type "done" to exit: ')
+        else:
+            answer = input('Enter "backwards", "random", "write poem to file" or "custom" to see the poem printed in that way. Or type "done" to exit: ')
     
 
-
-poem_list = get_file_lines("poem.txt")
-
-lines_printed_backwards(poem_list)
-
-lines_printed_random(poem_list)
-
-print(lines_printed_random(poem_list))
-
-print(lines_printed_custom(poem_list))
-
+stretch_menu()
